b868118d01c0fd83c8aa9996b8f4e57ee820199a
DESKTOP-QSIMVI3\jplee <jplee@solbox.com>
2022-12-27 14:45:28 +0900

    Initial commit

---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"strconv"
+	"time"
+
+	"github.com/segmentio/kafka-go"
+)
+
+const (
+	topic         = "message-log"
+	brokerAddress = "192.168.110.90:9092"
+)
+
+func createTopic(context context.Context) {
+	conn, err := kafka.DialLeader(context, "tcp", brokerAddress, topic, 0)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer conn.Close()
+}
+
+func producer(ctx context.Context) {
+	writer := kafka.NewWriter(kafka.WriterConfig{
+		Brokers:      []string{brokerAddress},
+		Topic:        topic,
+		Balancer:     &kafka.LeastBytes{},
+		BatchSize:    1,
+		BatchTimeout: 100 * time.Millisecond, //Control Sending messages time
+	})
+	defer writer.Close()
+
+	for i := 0; i < 10; i++ {
+		err := writer.WriteMessages(ctx, kafka.Message{
+			Key:   []byte("Key=" + strconv.Itoa(i)),
+			Value: []byte("Value=" + strconv.Itoa(i)),
+		})
+		if err != nil {
+			panic("could not write message " + err.Error())
+		}
+	}
+
+	fmt.Println("Producer finished...")
+}
+
+func consumer(ctx context.Context) {
+	reader := kafka.NewReader(kafka.ReaderConfig{
+		Brokers: []string{brokerAddress},
+		Topic:   topic,
+		GroupID: "my-group",
+	})
+
+	for {
+		msg, err := reader.ReadMessage(ctx) //Block and Wait
+		if err != nil {
+			panic("could not read message " + err.Error())
+		}
+
+		fmt.Println("Received: Key=", string(msg.Key), ", Value=", string(msg.Value))
+	}
+}
+
+func main() {
+	context := context.Background()
+
+	createTopic(context)
+	go producer(context)
+	consumer(context)
+}
